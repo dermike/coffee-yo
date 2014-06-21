f5dda7fcdbe15c7ff197a2843d20b087363e15ce
Mikael Jergefelt <mike@redvolume.com>
2014-06-21 16:46:05 +0200

    Gulp build script

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var uncss = require('gulp-uncss'),
+    concatCss = require('gulp-concat-css'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    minifycss = require('gulp-minify-css');
+
+gulp.task('css', function() {
+  gulp.src(['bower_components/foundation/css/foundation.css', 'css/app.css'])
+    .pipe(uncss({
+      html: ['index.html'], // Saved from full copy of DOM
+      ignore: ['.button', '.expand', '.sent']
+    }))
+    .pipe(concatCss("app.min.css"))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('scripts', function() {
+  gulp.src(['bower_components/jquery/dist/jquery.js', 'js/app.js'])
+    .pipe(uglify())
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('./js'));
+});
+
+gulp.task('default', ['css', 'scripts']);
